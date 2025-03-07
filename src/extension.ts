ac4b54df1297d0f4e07eceb85aa864046c673be3
Brandon Ly <brandonly@lostcoding.com>
2025-03-07 16:55:21 -0600

    Add react app and biome

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
+// TODO: We'll be using the Webview API to create our extension's regex playground.
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "regexer" is now active!');
@@ -13,14 +13,43 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('regexer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
-	});
+	const disposable = vscode.commands.registerCommand(
+		"regexer.helloWorld",
+		() => {
+			// The code you place here will be executed every time your command is executed
+			// Display a message box to the user
+			vscode.window.showInformationMessage("Hello World from Bran!");
+		},
+	);
 
 	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("regexer.startRegexer", async () => {
+			const panel = vscode.window.createWebviewPanel(
+				"regexer",
+				"Regexer",
+				vscode.ViewColumn.One,
+				{},
+			);
+
+			panel.webview.html = await getWebviewContent();
+		}),
+	);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+async function getWebviewContent() {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+	<h1>Hello World </h1>
+</body>
+</html>`;
+}
